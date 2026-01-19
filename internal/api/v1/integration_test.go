254a23d0fd3851a8e6fa1a1be2e7e729c172b556
Mark Mayo <mark@vmunix.com>
2026-01-18 17:06:46 -0800

    test: add mock Prowlarr server

--- internal/api/v1/integration_test.go
+++ internal/api/v1/integration_test.go
@@ -50,3 +50,27 @@ func (e *testEnv) cleanup() {
 		_ = e.db.Close()
 	}
 }
+
+func (e *testEnv) mockProwlarrServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/api/v1/search" {
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+		}
+
+		// Return configured releases
+		w.Header().Set("Content-Type", "application/json")
+		resp := make([]map[string]any, len(e.prowlarrReleases))
+		for i, rel := range e.prowlarrReleases {
+			resp[i] = map[string]any{
+				"title":       rel.Title,
+				"guid":        rel.GUID,
+				"indexer":     rel.Indexer,
+				"downloadUrl": rel.DownloadURL,
+				"size":        rel.Size,
+				"publishDate": rel.PublishDate.Format(time.RFC3339),
+			}
+		}
+		_ = json.NewEncoder(w).Encode(resp)
+	}))
+}
